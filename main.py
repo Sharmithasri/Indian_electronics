97a76d08c7be7752388e86ce2e597535d2866029
Lenovo-i5 <devap3784@gmail.com>
2026-05-31 12:50:16 +0530

    Updated billing and employee billing pages

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ app.secret_key = 'your_secret_key'
 
 
 
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
-app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Vdp_30664711'
+app.config['MYSQL_DB'] = 'indian'
+
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 mysql=MySQL(app)
 
